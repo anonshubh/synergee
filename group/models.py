e48d517d374126534ebe02c0dfcd1282aae5ad75
Shubh Pathak <shubhpathak07@gmail.com>
2021-01-10 01:47:00 +0530

    Added Member and Interest Model

--- group/models.py
+++ group/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Interest(models.Models):
+    """
+    Category of One's Interest
+    """
+    type = models.CharField(max_length=100)
+    familiarity = models.PositiveIntegerField(default=10)
+
+    def __str__(self):
+        return self.type
+    
+
+class Member(models.Model):
+    """
+    Information/Data of Group Member
+    """
+    name = models.CharField(max_length=56)
+    image = models.URLField()
+    email = models.EmailField(max_length=56)
+    interests = models.ManyToManyField(Interest)
+    github_url = models.URLField(blank=True,null=True)
+    linkedin_url = models.URLField(blank=True,null=True)
+
+    def __str__(self):
+        return str(self.email)
