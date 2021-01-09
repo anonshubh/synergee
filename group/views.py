37964fede2972a71c05dd2c227d2723eb3aba061
Shubh Pathak <shubhpathak07@gmail.com>
2021-01-10 01:51:35 +0530

    Added Boilerplate for Views

--- group/views.py
+++ group/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from .models import Member,Interest
+
+
+class ListProfile(View):
+    """
+    On GET: Returns the List of Profiles from Database
+    """
+
+    def get(self,request,format=None):
+        pass
+
+
+class DetailProfile(View):
+    """
+    On GET: Returns the Details on Specific Profile
+    """
+
+    def get(self,request,format=None):
+        pass
