f863a90c97ef30f8aaa0cb7c9b7b317c5f951258
Shubh Pathak <shubhpathak07@gmail.com>
2021-02-13 01:55:17 +0530

    Polished CSS

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     let receiver_div = document.querySelector('#div_id_entire_team');
-    receiver_div.classList.add('mt-2');
+    try{
+        receiver_div.classList.add('mt-2');
+        
+        let check_box = document.querySelector('#id_entire_team');
+        let receiver = document.querySelector('#div_id_receiver');
+            
+        check_box.addEventListener('change',()=>{
+            if(check_box.checked){
+                receiver.style.display = 'none';
+            }
+            else{
+                receiver.style.display = 'block';
+            }
+        })
+    }
+    catch{
+        console.log("Continued");
+    }
 
-    let check_box = document.querySelector('#id_entire_team');
-    let receiver = document.querySelector('#div_id_receiver');
+    let contactButton = document.querySelector('#contact-link');
 
-
-    check_box.addEventListener('change',()=>{
-        if(check_box.checked){
-            receiver.style.display = 'none';
-        }
-        else{
-            receiver.style.display = 'block';
-        }
-    })
-    
+    if (window.location.href != "http://127.0.0.1:8000/"){
+        contactButton.style.display = 'none';
+    }
 });
